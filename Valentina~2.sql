635c4e844489ec6365b9ac8feb5f4af119f1f668
valentina78 <valentina.lupulciuc@gmail.com>
2018-10-05 20:14:07 +0300

    2

---
+++ Valentina~2.sql
@@ -0,0 +1,5 @@
+--deschide fisier nou -tools
+
+-- save as - in fisier pe disc
+--git hub - intri in repositoriu creat
+--git-hub -- commit --sageata din stanga violet-- scrii comentarii fereastra dreapta - commit and push
